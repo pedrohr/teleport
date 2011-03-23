b92f2d4f0e52fcba3fcb2a58682a05a4315a46f9
Pedro Henrique Ribeiro de Assis <pedroh2306@gmail.com>
2011-03-22 22:47:40 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teletransport::Application.config.secret_token = '457000242d5cd51bdf3603507aa25d712d922b099eee3781c505275f8a1e7c16fbe9e9c5ea278cbe09be11e3db759af299725807b9afbdaf63ff858d29e4eafa'
