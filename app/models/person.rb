97aaeaa797568f39f0ab18968076f9ced1ee70ce
Pedro Henrique Ribeiro de Assis <pedroh2306@gmail.com>
2011-04-14 02:32:32 -0300

    Inserting the key generator before each sending, first version of the 'after_save' method

--- app/models/person.rb
+++ app/models/person.rb
@@ -57,47 +57,71 @@ class Teleport < Teleconfig
     end
   end
 
-  #ISSUE: dont need to send the whole JSON. Just the key.
+  def handle_record(record)
+    from = record.class.to_s
+    record = record.serializable_hash
+    record = record.merge({"__key" => record.to_s.crypt("id")})
+    return {from => record}.to_json
+  end
 
   def after_create(record)
-    json = record.to_json
+    json = handle_record(record)
     commit(json, POST)
   end
 
   def after_destroy(record)
-    json = record.to_json
+    json = handle_record(record)
     commit(json, DELETE)
   end
 
   def after_save(record)
+    #Condition to avoid the 'save' trigger after a 'create'
+    record.changed_attributes.each { |k,v|
+      return false if v == nil
+    }
+
+    #preparing updates
+    update = record.changed_attributes
+    update.each { |k,v|
+      update[k] = record[k]
+    }
+    update = {"update" => update}
+
+    #old_record for the key-generation proccess
+    old_record = record.dup
+    record.changes.each { |attr,values|
+      old_record[attr] = values.first
+    }
+    #generating the new key
+    key = old_record.serializable_hash.to_s.crypt("id") 
+
+    # decision:
+    # => a composed key: (__key + created_at) is not being
+    # => cuz it set let two or more sources change the same     # tuple in the central database.
+    record = update.merge({"__key"=> key})
     json = record.to_json
     commit(json, PUT)
   end
 
   def before_create(record)
-    json = record.to_json
-    commit(json, POST)
+    after_create(record)
   end
 
   def before_destroy(record)
-    json = record.to_json
-    commit(json, DELETE)
+    after_destroy(record)
   end
 
   def before_save(record)
-    json = record.to_json
-    commit(json, PUT)
+    after_save(record)
   end
 end
 
 class Person < ActiveRecord::Base
+  #this methods must be static to the model
   def self.my_after_create
   end
 
-  def self.my_after_destroy
-  end
-
   after_create Teleport.new(my_after_create)
-  after_destroy Teleport.new(my_after_destroy)
+  after_destroy Teleport.new(nil)
   after_save Teleport.new(nil)
 end
