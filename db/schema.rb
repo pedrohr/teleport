5f12e14561bcf86e7603890c7fe19754e3460709
Pedro Henrique Ribeiro de Assis <pedroh2306@gmail.com>
2011-05-21 23:33:43 -0300

    Renaming database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110323021630) do
+ActiveRecord::Schema.define(:version => 20110414032313) do
+
+  create_table "candidates", :force => true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.integer  "phone"
+    t.string   "occupation"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "__key"
+  end
 
   create_table "people", :force => true do |t|
     t.string   "name"
